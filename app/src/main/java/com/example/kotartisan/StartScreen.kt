d5e9eb2f7d551f087baea3beb5d0659825b41f6d
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-12-02 22:02:35 -0500

    style theme

--- app/src/main/java/com/example/kotartisan/StartScreen.kt
+++ app/src/main/java/com/example/kotartisan/StartScreen.kt
@@ -1,11 +1,18 @@
 package com.example.kotartisan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
+private lateinit var startButton: ImageButton
 
 class StartScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_screen)
+        startButton = findViewById(R.id.startButton)
+        startButton.setOnClickListener { val intent = Intent(this@StartScreen, Drawing::class.java)
+            startActivity(intent) }
     }
 }
